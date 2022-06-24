8c2e932330e199f9e5ce1bf0ab9d29a10f205737
the-new-kim <jonkim0309@gmail.com>
2022-06-25 06:27:46 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -4,7 +4,12 @@ const app = express();
 
 const PORT = 4000;
 
-const handleListen = () =>
-  console.log(`🚀Listening on http://localhost:${PORT} 🚀`);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 
-app.listen(PORT, handleListen);
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(PORT, () =>
+  console.log(`🚀Listening on http://localhost:${PORT} 🚀`)
+);
