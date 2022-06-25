eca417770d0e23a373a9dfb53288130d882342cc
the-new-kim <jonkim0309@gmail.com>
2022-06-26 06:27:32 +0900

    WebSockets in NodeJs

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocket } from "ws";
 import express from "express";
 
 const app = express();
@@ -8,8 +10,13 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(PORT, () =>
-  console.log(`🚀Listening on http://localhost:${PORT} 🚀`)
-);
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(PORT, () => {
+  console.log(`🚀Listening on http://localhost:${PORT} 🚀`);
+});
